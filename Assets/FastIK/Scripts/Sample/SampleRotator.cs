44ea30b707983b4bd465898ddaec84564b407a4d
marticoma <46682113+marticoma@users.noreply.github.com>
2020-05-23 18:38:00 +0200

    Initial commit

---
+++ Assets/FastIK/Scripts/Sample/SampleRotator.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace DitzelGames.FastIK
+{
+    public class SampleRotator : MonoBehaviour
+    {
+        
+        void Update()
+        {
+            //just rotate the object
+            transform.Rotate(Time.deltaTime * 90, Time.deltaTime * 90, Time.deltaTime * 90);
+        }
+    }
+}
---
+++ Assets/FastIK/Scripts/Sample/SampleSinMover.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Specialized;
+using UnityEngine;
+
+namespace DitzelGames.FastIK
+{
+    public class SampleSinMover : MonoBehaviour
+    {
+        public Vector3 Dir;
+        public Vector3 Start;
+
+        private void Awake()
+        {
+            Start = transform.position;
+        }
+
+        void Update()
+        {
+            //just move the object from a to b and back
+            transform.position = Start + Dir * Mathf.Sin(Time.timeSinceLevelLoad) * 0.6f + Vector3.up * Mathf.Cos(Time.timeSinceLevelLoad) * 0.2f
+                 + Vector3.right * Mathf.Sin(Time.timeSinceLevelLoad) * 0.2f;
+        }
+    }
+}
